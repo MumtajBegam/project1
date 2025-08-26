09e6da9aac32a893ef01b08d2dcd26e6aba2af08
MumtajBegam <mumtajbegamaalima@gmail.com>
2025-08-26 12:08:51 +0530

    Updated index.html

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import './Components/Login.css'
 import Pwd from './Components/Pwd'
 import './Components/Pwd.css'
 import Home from './Components/Home'
-import {Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
@@ -20,7 +20,7 @@ function App() {
         <Route path="/pwd" element={<Pwd/>} />
         <Route path="/signup" element={<Signup/>} />
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
